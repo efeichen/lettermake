05136a30a2f83996643774447c5c717ec394435b
Kenneth Chen <efeichen@gmail.com>
2019-08-17 01:30:57 -0700

    Implement cover letter template

--- src/components/Template.js
+++ src/components/Template.js
@@ -3,39 +3,49 @@ import { Page, Text, View, Document, StyleSheet } from '@react-pdf/renderer';
 
 const Template = ({ content }) => {
     return (
-        <Document>
+        <Document title={`${content.recipient} Cover Letter`}>
             <Page style={styles.body} size='LETTER'>
-                <View style={styles.author}>
-
+                <View style={styles.p}>
+                    <Text>{content.author}</Text>
+                    <Text>{content.address}</Text>
+                    <Text>{content.email}</Text>
+                    <Text>Cell: {content.phone}</Text>
                 </View>
-                <View style={styles.recipient}>
-
+                <View style={styles.p}>
+                    <Text>{content.recipient}</Text>
+                    <Text>{content.recipientAddress}</Text>
                 </View>
-                <View style={styles.subject}>
-
+                <View style={styles.p}>
+                    <Text>RE: {content.subject}</Text>
                 </View>
-                <View style={styles.date}>
-
+                <View style={styles.p}>
+                    <Text>{content.date}</Text>
                 </View>
-                <View style={styles.salute}>
-
+                <View style={styles.p}>
+                    <Text>Dear {content.recipient},</Text>
                 </View>
-                <View style={styles.content}>
+                <View style={styles.p}>
                     <Text>
-                        Hi my name is {content.author}. {content.personalization}
+                        Hi my name is {content.author}. {content.interest}
                     </Text>
-                    <Text>
-                        {content.interest}
-                    </Text>
+                </View>
+                <View style={styles.p}>
+                    <Text>{content.skills}</Text>
+                </View>
+                <View style={styles.p}>
                     <Text>
                         I have attached a copy of my resume which details my skills and achievements. Please let me know what would be the best way to discuss my qualifications and how I could benefit the research conducted by your group.
                     </Text>
+                </View >
+                <View style={styles.p}>
                     <Text>
                         If you have any further questions, please don’t hesitate to contact me at {content.phone} or {content.email}. Thank you for your time and consideration and I look forward to speaking with you.
                     </Text>
                 </View>
-                <View>
+                <View style={styles.p}>
                     <Text>Sincerely,</Text>
+                </View>
+                <View style={styles.p}>
                     <Text>{content.author}</Text>
                 </View>
             </Page>
@@ -44,6 +54,17 @@ const Template = ({ content }) => {
 }
 
 const styles = StyleSheet.create({
+    body: {
+        padding: '1in',
+        fontFamily: 'Times-Roman',
+        fontSize: '11pt',
+        lineHeight: '1.15pt'
+    },
+    p: {
+        marginBottom: '11pt'
+    }
 });
+
+// Font.register()
 
 export default Template;
