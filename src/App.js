abb0217d58d9e72637dbb4920abd1250fa9d32ea
Kenneth Chen <efeichen@gmail.com>
2019-08-17 01:30:32 -0700

    Update letter fields

--- src/App.js
+++ src/App.js
@@ -2,30 +2,31 @@ import React, { useState } from 'react';
 import Home from 'views/Home';
 import Reader from 'views/Reader';
 import About from 'views/About';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 const App = () => {
     const [letter, setLetter] = useState({
         author: '',
         email: '',
-        address: '',
-        portfolio: '',
-        content: '',
-        edu: [],
-        exp: [],
-        projects: [],
-        skills: []
+        authorAddress: '',
+        phone: '',
+        recipient: '',
+        recipientAddress: '',
+        subject: '',
+        date: '',
+        interest: '',
+        skills: ''
     });
 
     return (
         <Router>
             <Route exact={true} path='/' render={() => (
-                <Home setContent={setLetter}/>
-            )}/>
+                <Home setContent={setLetter} />
+            )} />
 
             <Route exact={true} path='/reader' render={() => (
                 <Reader content={letter} />
-            )}/>
+            )} />
             <Route exact={true} path='/about' component={About} />
         </Router>
     );
